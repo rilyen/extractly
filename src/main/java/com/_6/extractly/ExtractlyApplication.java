a9f0f72db9a276940e4399bd58dee3761fa2b01c
rilyen <aprilvnguyen@gmail.com>
2026-06-20 19:00:11 -0700

    initial commit

---
+++ src/main/java/com/_6/extractly/ExtractlyApplication.java
@@ -0,0 +1,13 @@
+package com._6.extractly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_6/extractly/ExtractlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.extractly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
