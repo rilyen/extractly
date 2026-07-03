40e7faebbac0baaadd6f9f922c66a97e1906f47e
Taj <tajveergill11@gmail.com>
2026-07-03 05:46:03 -0700

    user entity constructors added

--- src/main/java/com/_6/extractly/models/User.java
+++ src/main/java/com/_6/extractly/models/User.java
@@ -16,14 +16,19 @@ public class User {
     private String password;
 
     @Enumerated(EnumType.STRING)
+     @Column(nullable = false)
     private Role role;
+
+    public User() {}
+
+    public User(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getEmail() {
